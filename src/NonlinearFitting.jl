a1a218e5490de03004b87914d54690c8c8f454e8
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-08-28 11:09:21 +0000

    Snapshot of branch master taken on 2019-08-28T07:09:20.315-04:00 from "https://github.com/UnofficialJuliaMirror/EquationsOfState.jl-0438fec6-b81c-52d9-9651-d286848f50a8"

--- src/NonlinearFitting.jl
+++ src/NonlinearFitting.jl
@@ -13,13 +13,13 @@ module NonlinearFitting
 
 using LsqFit: curve_fit
 
-using EquationsOfState.Targets
+using EquationsOfState
 using EquationsOfState.Collections
 
 export lsqfit
 
 function lsqfit(
-    A::Type{<:EquationOfStateTarget},
+    A::Type{<:EquationOfStateRelation},
     eos::E,
     xdata::Vector{T},
     ydata::Vector{T};
@@ -29,8 +29,21 @@ function lsqfit(
     fitted = curve_fit(model, xdata, ydata, collect(eos); kwargs...)
     debug ? fitted : E(fitted.param)
 end  # function lsqfit
+"""
+    lsqfit(T, eos, xdata, ydata; debug = false, kwargs...)
+
+Fit an equation of state using least-squares fitting method (with the Levenberg-Marquardt algorithm).
+
+# Arguments
+- `T::Type{<:EquationOfStateRelation}`: an `EquationOfStateRelation`. If it is `EnergyRelation`, fit ``E(V)``; if `PressureRelation`, fit ``P(V)``; if `BulkModulusRelation`, fit ``B(V)``.
+- `eos::EquationOfState`: a trial equation of state.
+- `xdata::AbstractVector`: a vector of volumes.
+- `ydata::AbstractVector`: a vector of energies, pressures, or bulk moduli.
+- `debug::Bool=false`: if `true`, then an `LsqFit.LsqFitResult` is returned, containing estimated Jacobian, residuals, etc.; if `false`, a fitted `EquationOfState` is returned. The default value is `false`.
+- `kwargs`: the rest keyword arguments that will be sent to `LsqFit.curve_fit`. See its [documentation](https://github.com/JuliaNLSolvers/LsqFit.jl/blob/master/README.md).
+"""
 function lsqfit(
-    A::Type{<:EquationOfStateTarget},
+    A::Type{<:EquationOfStateRelation},
     eos::E,
     xdata::X,
     ydata::Y;
